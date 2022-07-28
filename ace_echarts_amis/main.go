83c5302d4828bf42c649766378d08f3a35694230
shanks <shanks@onepiece.ai>
2022-07-28 13:16:11 +0800

    fix: 增加amis+ace示例

--- ace_echarts_amis/main.go
+++ ace_echarts_amis/main.go
@@ -6,19 +6,25 @@ import (
 	"github.com/yosssi/ace"
 )
 
+const defaultJson = `
+
+{"type":"page","title":"default","body":[{"type":"chart","config":{"xAxis":{"type":"category","data":["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]},"yAxis":{"type":"value"},"series":[{"data":[820,932,901,934,1290,1330,1320],"type":"line"}]},"replaceChartOption":true,"id":"u:5897ec2e03fe","dataFilter":"","api":""}],"id":"u:c508a28b833b"}
+`
+
 func main() {
 	r := gin.Default()
 
 	r.Static("/assets", "./")
 
-	tpl, err := ace.Load("default", "", nil)
+	tpl, err := ace.Load("default", "", &ace.Options{DynamicReload: true})
 	if err != nil {
 		fmt.Println("EOF:", err)
 		return
 	}
-
 	r.GET("/", func(c *gin.Context) {
-		_ = tpl.Execute(c.Writer, nil)
+		_ = tpl.Execute(c.Writer, map[string]interface{}{
+			"pageJson": defaultJson,
+		})
 	})
 	_ = r.Run() // listen and serve on 0.0.0.0:8080
 }
